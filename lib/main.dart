7071411314976794541213cd665334e0227e0fbc
aparnapanda55 <aparnapanda55@gmail.com>
2021-12-30 20:19:20 +0530

    styled the result page and search

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
       title: 'Youtube Downloader',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Youtube Downloader'),
+          title: const Text('Youtube Downloader'),
         ),
-        body: HomePage(),
+        body: const HomePage(),
       ),
     );
   }
@@ -28,62 +28,116 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const TextField(
-          decoration: InputDecoration(
-            labelText: 'Paste Youtube link',
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        ElevatedButton(
-          onPressed: () {},
-          child: const Text('Download'),
-        ),
-        Expanded(
-          child: Row(
-            children: [
-              Container(
-                width: 500,
-                child: const Image(
-                  image:
-                      NetworkImage('https://i.ytimg.com/vi/VDqGoiOLld4/0.jpg'),
+    return Container(
+      margin: const EdgeInsets.all(30),
+      child: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
                 ),
+                borderSide: BorderSide(color: Colors.blue, width: 7.0),
               ),
-              Column(
+              labelText: 'Paste Youtube link',
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('Search'),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const ResultPane()
+        ],
+      ),
+    );
+  }
+}
+
+class ResultPane extends StatelessWidget {
+  const ResultPane({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[200],
+      ),
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 188,
+            width: 336,
+            child: Placeholder(
+              fallbackHeight: 100,
+              fallbackWidth: 200,
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Total Chaos | Trashy Thursday',
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('The Satya Show'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text('12.03'),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
                 children: [
-                  Text('Title'),
-                  Text('Channel name'),
-                  Text('duration'),
-                  Row(
-                    children: [
-                      DropdownButton<String>(
-                        items: const [
-                          DropdownMenuItem(
-                            value: 'a',
-                            child: Text('a'),
-                          ),
-                          DropdownMenuItem(
-                            value: 'ab',
-                            child: Text('ab'),
-                          ),
-                          DropdownMenuItem(
-                            value: 'ad',
-                            child: Text('ad'),
-                          ),
-                        ],
-                        onChanged: (value) {},
+                  DropdownButton<String>(
+                    items: const [
+                      DropdownMenuItem(
+                        value: 'asssssss',
+                        child: Text('assssssssssss'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'ab',
+                        child: Text('ab'),
+                      ),
+                      DropdownMenuItem(
+                        value: 'ad',
+                        child: Text('ad'),
                       ),
                     ],
-                  )
+                    onChanged: (value) {},
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Download'),
+                  ),
                 ],
               )
             ],
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
