fe7443618ea11b0460ba5f08161eb23ea592f850
aparnapanda55 <aparnapanda55@gmail.com>
2022-01-21 18:36:29 +0530

    download functionality done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -191,7 +192,6 @@ class _HomePageState extends State<HomePage> {
     final state = formKey.currentState;
     if (state == null || !state.validate()) return;
     final v = parseVideoId(url);
-    print('fetching details for $v');
     setState(() {
       videoId = v;
     });
@@ -265,14 +265,17 @@ class AdaptiveResultPane extends StatelessWidget {
                     flex: 4,
                     child: Thumbnail(thumbnailData: video.thumbnailData),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 10,
                   ),
                   Expanded(
                     flex: 5,
-                    child: DownloadDetails(
-                      video: video,
-                      vertical: false,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: DownloadDetails(
+                        video: video,
+                        vertical: false,
+                      ),
                     ),
                   ),
                 ],
@@ -312,6 +315,7 @@ class DownloadDetails extends StatelessWidget {
           height: 10,
         ),
         DownloadMenu(
+          title: video.title,
           items: video.videos,
           label: 'Video',
         ),
@@ -319,9 +323,15 @@ class DownloadDetails extends StatelessWidget {
           height: 10,
         ),
         DownloadMenu(
+          title: video.title,
           items: video.audios,
           label: 'Audio',
-        )
+        ),
+        const SizedBox(height: 10),
+        Text(
+          'If the browser starts to play the media instead of downloading, you can save it with ctrl+s, cmd+s or phones Save As button.',
+          style: Theme.of(context).textTheme.caption,
+        ),
       ],
     );
   }
@@ -330,10 +340,12 @@ class DownloadDetails extends StatelessWidget {
 class DownloadMenu<T extends UrlItem> extends StatefulWidget {
   const DownloadMenu({
     Key? key,
+    required this.title,
     required this.items,
     required this.label,
   }) : super(key: key);
 
+  final String title;
   final List<T> items;
   final String label;
 
@@ -393,7 +405,12 @@ class _DownloadMenuState<T extends UrlItem> extends State<DownloadMenu<T>> {
         ),
         IconButton(
           iconSize: 30,
-          onPressed: () {},
+          onPressed: () {
+            final fileName = '${widget.title}.${selectedItem.format}';
+            final a = AnchorElement(href: selectedItem.url);
+            a.download = fileName;
+            a.click();
+          },
           icon: const Icon(Icons.download),
         ),
       ],
@@ -408,7 +425,7 @@ class Thumbnail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.only(left: 16.0),
       child: Image.memory(const Base64Decoder().convert(thumbnailData)),
     );
   }
